e953e51f71f2c6f38f898934c7412b0303c7adde
FelipenKniess <felipe.nkniess@gmail.com>
2021-07-05 20:38:16 -0300

    Criada todas as rotas da aplicaçao

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
-
 import { AuthContextProvider } from '../contexts/AuthContext';
 
 import Home from '../pages/Home';
 import NewRoom from '../pages/NewRoom';
 import Room from '../pages/Room';
+import AdminRoom from '../pages/AdminRoom';
 
 export default function Routes() {
     return (
@@ -16,7 +16,7 @@ export default function Routes() {
                     <Route path="/" exact component={Home} />
                     <Route path="/rooms/new" component={NewRoom} />
                     <Route path="/rooms/:id" component={Room} />
-                    {/* <Route path="/admin/rooms/:id" component={AdminRoom} /> */}
+                    <Route path="/admin/rooms/:id" component={AdminRoom} />
                 </Switch>
             </AuthContextProvider>
         </BrowserRouter>
